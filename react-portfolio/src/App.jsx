2425f31673cb47c3afb1cd1829d42a7fbcd202f9
Edwin0204 <edwins0204@gmail.com>
2022-08-06 17:57:05 -0500

    updates

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'  
-import Services from './components/services/Services'
+import Languages from './components/Languages/Languages'
 import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -14,9 +13,8 @@ const App = () => {
         <Header /> 
         <Nav />
         <About />
-        <Services />
+        <Languages />
         <Portfolio />
-        <Testimonials />
         <Contact />
         <Footer />
                  
---
+++ react-portfolio/src/components/Languages/Languages.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './languages.css'
+
+const Languages = () => {
+  return (
+    <section id='languages'>Languages</section>
+  )
+}
+
+export default Languages

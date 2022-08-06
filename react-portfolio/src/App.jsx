e4f76f8df90a2222591fb35055b409fe45e43b75
Edwin0204 <edwins0204@gmail.com>
2022-08-06 17:52:33 -0500

    media query for about

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
+import About from './components/about/About'  
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Testimonials from './components/testimonials/Testimonials'
@@ -15,7 +14,6 @@ const App = () => {
         <Header /> 
         <Nav />
         <About />
-        <Experience />
         <Services />
         <Portfolio />
         <Testimonials />

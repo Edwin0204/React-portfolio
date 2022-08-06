7eac03e74cb53c32a17a49070e4f859e0be9fdd7
Edwin0204 <edwins0204@gmail.com>
2022-08-05 20:51:53 -0500

    nav barr

--- react-portfolio/src/components/nav/Nav.jsx
+++ react-portfolio/src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {IoIosHome} from 'react-icons/io'
+import {SiAboutdotme} from 'react-icons/si'
+import {BsFillFileEarmarkCodeFill} from 'react-icons/bs'
+import {MdOutlineLanguage} from 'react-icons/md'
+import {RiContactsBook2Fill} from 'react-icons/ri'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><IoIosHome/></a>
+      <a href='#about'><SiAboutdotme/></a>
+      <a href='#experience'><BsFillFileEarmarkCodeFill/></a>
+      <a href='#services'><MdOutlineLanguage/></a>
+      <a href='#contact'><RiContactsBook2Fill/></a>
+    </nav>
   )
 }
 

97e7ac3adcae2ba68f70baef60c9303083bc77d0
Edwin0204 <edwins0204@gmail.com>
2022-08-05 21:20:58 -0500

    useState on navbarr

--- react-portfolio/src/components/nav/Nav.jsx
+++ react-portfolio/src/components/nav/Nav.jsx
@@ -5,15 +5,17 @@ import {SiAboutdotme} from 'react-icons/si'
 import {BsFillFileEarmarkCodeFill} from 'react-icons/bs'
 import {MdOutlineLanguage} from 'react-icons/md'
 import {RiContactsBook2Fill} from 'react-icons/ri'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState ('#')
   return (
     <nav>
-      <a href='#' className='active'><IoIosHome/></a>
-      <a href='#about'><SiAboutdotme/></a>
-      <a href='#experience'><BsFillFileEarmarkCodeFill/></a>
-      <a href='#services'><MdOutlineLanguage/></a>
-      <a href='#contact'><RiContactsBook2Fill/></a>
+      <a href='#' onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active':''}><IoIosHome/></a>
+      <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active':''}><SiAboutdotme/></a>
+      <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active':''}><BsFillFileEarmarkCodeFill/></a>
+      <a href='#services' onClick={()=> setActiveNav('#services')} className={activeNav === '#services' ? 'active':''}><MdOutlineLanguage/></a>
+      <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active':''}><RiContactsBook2Fill/></a>
     </nav>
   )
 }

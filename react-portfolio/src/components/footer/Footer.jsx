033f419a87d060bc867d537ab2b500c25aa9ac9b
Edwin0204 <edwins0204@gmail.com>
2022-08-07 11:12:34 -0500

    footer

--- react-portfolio/src/components/footer/Footer.jsx
+++ react-portfolio/src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {GrLinkedin} from 'react-icons/gr'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'></a>
+
+      <ul className='links'>
+      <li><a href="#">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#languages">Languages</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+      <a href='https://www.linkedin.com/in/edwin-simpson-50948052/' target="_blank" className='socials__icon'><GrLinkedin/></a>
+      <a href='https://github.com/Edwin0204' target="_blank" className='socials__icon'><FaGithub/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Edwin Simpson. All rights reserved.</small>
+      </div>
+
+    </footer>
   )
 }
 
